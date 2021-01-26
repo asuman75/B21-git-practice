dcf11ab0f5479229590cd9ea70328da57d68b83c
asuman75 <74166822+asuman75@users.noreply.github.com>
2021-01-26 15:22:58 -0500

    movie1 updated more

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello word,B21");
         System.out.println("B21 future SDET leaning Git");
+  //watching more movie
+
 
     }
 
